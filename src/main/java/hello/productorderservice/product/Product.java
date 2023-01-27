cc906e570e45886c90559feeeb274e7371214847
kimjinwook <jinwook628@gmail.com>
2023-01-27 21:36:26 +0900

    JPA 적용하기

--- src/main/java/hello/productorderservice/product/Product.java
+++ src/main/java/hello/productorderservice/product/Product.java
@@ -1,12 +1,23 @@
 package hello.productorderservice.product;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String name;
-    private final int price;
-    private final DiscountPolicy discountPolicy;
+    private String name;
+    private int price;
+    private DiscountPolicy discountPolicy;
 
     public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
@@ -18,11 +29,4 @@ class Product {
         this.discountPolicy = discountPolicy;
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
--- src/main/java/hello/productorderservice/product/ProductRepository.java
+++ src/main/java/hello/productorderservice/product/ProductRepository.java
@@ -1,18 +1,8 @@
 package hello.productorderservice.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Repository
-class ProductRepository {
-
-    private Long sequence = 0L;
-    private Map<Long, Product> persistence = new HashMap<>();
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
+interface ProductRepository extends JpaRepository<Product, Long> {
 }

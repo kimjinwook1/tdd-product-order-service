7193f57908c8e1eadf3f00c4f77237a163a2249a
kimjinwook <jinwook628@gmail.com>
2023-01-28 15:01:09 +0900

    API 테스트로 전환하기

--- src/test/java/hello/productorderservice/product/ProductServiceApiTest.java
+++ src/test/java/hello/productorderservice/product/ProductServiceApiTest.java
@@ -1,13 +1,18 @@
 package hello.productorderservice.product;
 
 import hello.productorderservice.ApiTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ProductServiceApiTest extends ApiTest {
 
+    @Autowired
+    ProductRepository productRepository;
 
     @Test
     void 상품등록() {
@@ -27,6 +32,17 @@ class ProductServiceApiTest extends ApiTest {
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+    }
+
+    @Test
+    void 상품수정() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+
+        final long productId = 1L;
+        final ExtractableResponse<Response> response = ProductSteps.상품수정요청(productId);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(productRepository.findById(1L).get().getName()).isEqualTo("상품 수정");
     }
 
 }

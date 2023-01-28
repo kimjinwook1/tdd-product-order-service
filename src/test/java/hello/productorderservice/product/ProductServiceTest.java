4982b511e17a4ee152fdbc4fa38ec3296b9556e0
kimjinwook <jinwook628@gmail.com>
2023-01-28 14:41:44 +0900

    Mockito 상품 수정 기능 테스트

--- src/test/java/hello/productorderservice/product/ProductServiceTest.java
+++ src/test/java/hello/productorderservice/product/ProductServiceTest.java
@@ -2,16 +2,21 @@ package hello.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     private ProductService productService;
-    private StubProductPort productPort = new StubProductPort();
+    private ProductPort productPort;
 
     @BeforeEach
     void setUp() {
+        productPort = Mockito.mock(ProductPort.class);
         productService = new ProductService(productPort);
     }
 
@@ -20,7 +25,7 @@ public class ProductServiceTest {
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort.getProduct_will_return = product;
+        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 

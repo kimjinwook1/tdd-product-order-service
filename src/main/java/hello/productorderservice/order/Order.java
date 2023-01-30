dcba29954783310228ed664d98f980fff66745ac
kimjinwook <jinwook628@gmail.com>
2023-01-30 23:18:33 +0900

    JPA 적용하기

--- src/main/java/hello/productorderservice/order/Order.java
+++ src/main/java/hello/productorderservice/order/Order.java
@@ -1,12 +1,25 @@
 package hello.productorderservice.order;
 
 import hello.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         this.product = product;
@@ -15,11 +28,4 @@ class Order {
         Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
